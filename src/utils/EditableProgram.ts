368e7de9e05e569a01aa7e05c06ce6726189265a
Michael Braun <michael@web4brauns.de>
2020-09-26 00:20:30 +0200

    Add methods for EditableProgram

--- src/utils/EditableProgram.ts
+++ src/utils/EditableProgram.ts
@@ -1,5 +1,76 @@
 import Program from "./Program";
+import { EventEmitter } from "events";
+import throttle from "lodash.throttle";
+import { ProgramNode } from "..";
+
+type NodeSocket = {
+    node: string,
+    socket: string,
+};
 
 export default class EditableProgram extends Program {
+    readonly #events = new EventEmitter();
 
+    #moveCache: { [node: string]: { x: number, y: number, throttle: () => void } } = {};
+
+    moveNode(nodeId: string, { diffX, diffY }: { diffX: number, diffY: number }) {
+        if (!this.#moveCache[nodeId]) {
+            this.#moveCache[nodeId] = {
+                x: 0,
+                y: 0,
+                throttle: throttle(() => {
+                    this.updateNode(nodeId, (node) => ({
+                        ...node,
+                        position: [node.position[0] + this.#moveCache[nodeId].x, node.position[1] + this.#moveCache[nodeId].y],
+                    }));
+                    this.#moveCache[nodeId].x = 0;
+                    this.#moveCache[nodeId].y = 0;
+
+                    this.#events.emit('update-node', { node: nodeId });
+                }, 25),
+            }
+        }
+
+        this.#moveCache[nodeId].x += diffX;
+        this.#moveCache[nodeId].y += diffY;
+        this.#moveCache[nodeId].throttle();
+    }
+
+    connectSockets(node1: NodeSocket, node2: NodeSocket) {
+        this.updateNode(node1.node, (programNode: ProgramNode) => {
+            return {
+                ...programNode,
+                outputs: {
+                    ...programNode.outputs,
+                    [node1.socket]: {
+                        connections: [...(programNode.outputs[node1.socket]?.connections || []), {
+                            node: node2.node,
+                            input: node2.socket,
+                        }],
+                    }
+                }
+            }
+        });
+
+        this.updateNode(node1.node, (programNode: ProgramNode) => {
+            return {
+                ...programNode,
+                inputs: {
+                    ...programNode.inputs,
+                    [node2.socket]: {
+                        connections: [...(programNode.inputs[node2.socket]?.connections || []), {
+                            node: node1.node,
+                            output: node1.socket,
+                        }],
+                    }
+                }
+            }
+        });
+
+        this.#events.emit('update-program');
+    }
+
+    get events() {
+        return this.#events;
+    }
 }
--- src/utils/Program.ts
+++ src/utils/Program.ts
@@ -1,9 +1,22 @@
-import { ProgramDefinition, ProgramNode } from "..";
+import { OutputSocketConnection, ProgramDefinition, ProgramNode, SocketConnection, SocketConnections } from "..";
+
+export type ConnectionInfo = {
+    output: {
+        node: string;
+        socket: string;
+    },
+    input: {
+        node: string;
+        socket: string;
+    }
+}
 
 export default class Program {
-    readonly #program: ProgramDefinition;
+    #program: ProgramDefinition;
 
     #nodeCache: ProgramNode[] | null = null;
+
+    #connectionCache: ConnectionInfo[] | null = null;
 
     constructor(program: ProgramDefinition) {
         this.#program = program;
@@ -21,7 +34,48 @@ export default class Program {
         return this.#program.nodes[id];
     }
 
+    updateNode(id: string, cb: (node: ProgramNode) => ProgramNode) {
+        const newNode = cb(this.#program.nodes[id]);
+
+        if (newNode) {
+            this.#program = {
+                ...this.#program,
+                nodes: {
+                    ...this.#program.nodes,
+                    [id]: newNode,
+                },
+            }
+            this.invalidateCache();
+        }
+    }
+
+    getConnections() {
+        if (!this.#connectionCache) {
+            const cache: ConnectionInfo[] = [];
+            this.getNodes()?.forEach((node) => {
+                Object.entries(node.outputs).forEach(([output, connections]) => {
+                    connections.connections.forEach((connection) => {
+                        cache.push({
+                            output: {
+                                node: String(node.id),
+                                socket: output,
+                            },
+                            input: {
+                                node: String(connection.node),
+                                socket: connection.input
+                            }
+                        });
+                    });
+                });
+            });
+            this.#connectionCache = cache;
+        }
+
+        return this.#connectionCache;
+    }
+
     private invalidateCache() {
         this.#nodeCache = null;
+        this.#connectionCache = null;
     }
 }
